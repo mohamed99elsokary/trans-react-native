b74a7ba271930a31773fbec3fa485553f84a07b3
mohamed99elsokary <mohamed99elsokary@gmail.com>
2022-12-10 23:16:11 +0200

    add some images

--- components/AlbumList.py
+++ components/AlbumList.py
@@ -1,27 +1,19 @@
-from Component_py.stubs import require, console, __pragma__  # __:skip
-from Component_py.component import Component
+from core.stubs import require, console, fetch, __pragma__  # __:skip
 from components.AlbumDetail import AlbumDetail
+
 
 React = require("react")
 ScrollView = require("react-native").ScrollView
 Text = require("react-native").Text
-
-__pragma__("skip")
-
-
-def fetch(s):
-    return None
-
-
-__pragma__("noskip")
+axios = require("axios")
 
 
 def AlbumList(props):
+    asd = "asd"
     return __pragma__(
         "js",
         "{}",
         """ (
-                <Text>AlbumList</Text>
-
+                <Text>{asd}</Text>
         ); """,
     )
