bd1a1a6001c7f8c507d586260965d15412f14058
mohamed99elsokary <mohamed99elsokary@gmail.com>
2022-12-13 06:03:23 +0200

    add navigation

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from core.stubs import require, __pragma__, module  # __:skip
+from core.navigation import Stack, NavigationContainer
 from native.screens.Auth.login import Login
+from native.screens.Auth.sign_up import Register
 
 React = require("react")
 View = require("react-native").View
@@ -10,9 +12,22 @@ def App():
         "js",
         "{}",
         """ (
-        <View style={{ "flex": 1 }}>
-            <Login />
-        </View>
+		<View style={{ flex: 1 }}>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName="login">
+					<Stack.Screen
+						options={{ headerShown: false }}
+						component={Login}
+						name="login"
+					/>
+                    <Stack.Screen
+						options={{ headerShown: false }}
+						component={Register}
+						name="register"
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</View>
     ); """,
     )
 
---
+++ core/navigation.py
@@ -0,0 +1,20 @@
+from core.stubs import __pragma__  # __:skip
+
+__pragma__(
+    "js",
+    "{}",
+    """
+import {
+     NavigationContainer as NC
+} from "@react-navigation/native";
+import {
+     createStackNavigator 
+} from "@react-navigation/stack";
+export var NavigationContainer = NC;
+export var Stack = createStackNavigator ();
+
+    """,
+)
+__pragma__("skip")
+Stack = False
+NavigationContainer = False
