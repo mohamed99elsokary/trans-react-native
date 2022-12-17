269cca49c4ee3875fa31a8bc96b34ec5d6aaf586
mohamed99elsokary <mohamed99elsokary@gmail.com>
2022-12-17 23:19:42 +0200

    update login method to be calling the login endpoint

--- project/screens/Auth/login.py
+++ project/screens/Auth/login.py
@@ -1,6 +1,8 @@
 from js_libs.stubs import require, __pragma__  # __:skip
 from js_libs.stubs import handleChange, validate
 from js_libs.react import React, useState
+from js_libs.requests import requests
+from project.base_data import BASE_URL
 from js_libs.react_native import (
     react_native,
     View,
@@ -17,10 +19,35 @@ Formik = require("formik")
 
 
 def Login(props):
-    def handelLogin(form):
-        props.navigation.navigate("register")
+    missing, set_missing = useState([])
+    form_dict, set_form_dict = useState({})
+    response, set_response = useState({})
 
-    def image():
+    def login(form_dict):
+        requests(
+            set_response,
+            {
+                "method": "POST",
+                "url": BASE_URL + "/users/login/",
+                "headers": {},
+                "data": form_dict,
+            },
+        )
+        if response["data"]:
+            print((response["data"]["token"]["access_token"]))
+        return response
+
+    def Submit(form_dict):
+        missing = validate(["phone", "password"], form_dict)
+        if len(missing) == 0 and len(form_dict) != 0:
+            token = login(form_dict)
+            # print(token["data"]["token"]["access_token"])
+        return set_missing(missing)
+
+        # props.navigation.navigate("register")
+
+    # JSX
+    def text_handler():
         persons = [
             {"id": 1, "name": "POS", "show": False},
             {"id": 2, "name": "Khusm", "show": True},
@@ -60,7 +87,7 @@ def Login(props):
 				style={styles.Logo}
 				source={require("../project/assets/Khusm.png")}
 			/>
-			 { image() }
+			 { text_handler() }
 			<View style={styles.formik}>
 				<Text style={{ fontSize: 14, fontWeight: "bold", color: "#2caca4" }}>
 					Phone
@@ -68,15 +95,13 @@ def Login(props):
 				<TextInput
 					keyboardType="numeric"
 					style={styles.input}
-					// value={values.phone}
-					// onChangeText={handleChange("phone")}
-					// onBlur={() => setFieldTouched("phone")}
 					placeholder="Phone"
 					placeholderTextColor="#2caca4"
+                    onChangeText={(x) => handleChange(form_dict,x,"phone")}
+
 				/>
-				{/* {touched.phone && errors.phone && (
-					<Text style={styles.erorrText}>{errors.phone}</Text>
-				)} */}
+                    {missing.includes("phone") && <Text>This field is required</Text>}
+
 
 				<Text
 					style={{
@@ -90,16 +115,17 @@ def Login(props):
 				</Text>
 				<TextInput
 					style={styles.input}
-					// value={values.password}
-					// onChangeText={handleChange("password")}
-					// onBlur={() => setFieldTouched("password")}
 					placeholder="Password"
 					secureTextEntry={true}
 					placeholderTextColor="#2caca4"
+                    onChangeText={(x) => handleChange(form_dict,x,"password")}
+
 				/>
+                {missing.includes("password") && <Text>This field is required</Text>}
+                
 				{/* Login  button */}
 
-				<TouchableOpacity onPress={handelLogin} style={styles.button}>
+				<TouchableOpacity onPress={()=>Submit(form_dict)} style={styles.button}>
 					<Text style={styles.buttonText}> LOGIN </Text>
 				</TouchableOpacity>
 			</View>
